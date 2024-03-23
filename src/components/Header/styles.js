eefae8d57bf6d51247e3427c9ff9c6c18e37b72f
joaohenrik03 <jh.signori@gmail.com>
2024-03-22 21:08:14 -0300

    :bug: fix header styles

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,8 +2,10 @@ import styled from "styled-components/native";
 
 export const HeaderContainer = styled.SafeAreaView`
   height: 50px;
+  width: 100%;
   align-items: center;
   justify-content: center;
+  padding: 8px;
   background-color: #323238;
 `;
 
@@ -12,4 +14,3 @@ export const Title = styled.Text`
   color: #fff;
   font-weight: bold;
 `;
-
