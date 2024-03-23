2473c6cd9ac1f944bea82f6fbe3ab3ab440e2b69
joaohenrik03 <jh.signori@gmail.com>
2024-03-22 21:58:47 -0300

    :sparkles: add update profile function

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,15 +1,154 @@
-import { Button } from "react-native";
-import { Header } from "../../components/Header";
-import { ProfileContainer } from "./styles";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Modal, Platform } from "react-native";
 import { AuthContext } from "../../Contexts/AuthContext";
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+import { launchImageLibrary } from 'react-native-image-picker';
+import Feather from 'react-native-vector-icons/Feather';
+import { Header } from "../../components/Header";
 import { StartLoading } from "../../components/StartLoading";
+import { 
+  BackButton, 
+  Button, 
+  ButtonText, 
+  UserEmailText, 
+  Input, 
+  ModalContainer, 
+  UserNameText, 
+  ProfileContainer, 
+  UploadImageButton, 
+  UploadImageText, 
+  UserAvatar 
+} from "./styles";
 
 export function Profile() {
-  const { onSignOut, fullLoading } = useContext(AuthContext);
+  const { 
+    onSignOut, 
+    fullLoading, 
+    user, 
+    setUser, 
+    setUserInLocalStorage 
+  } = useContext(AuthContext);
+
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [newNameText, setNewNameText] = useState('');
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getAvatar() {
+      try {
+        if (isActive) {
+          const response = await storage()
+            .ref('users')
+            .child(user.uid)
+            .getDownloadURL();
+
+          setAvatarUrl(response);
+        }
+      } catch {
+        setAvatarUrl(null);
+      }
+    }
+
+    getAvatar();
+  }, []);
 
   function handleSignOut() {
     onSignOut();
+  }
+
+  function handleUpdatePhotoProfile() {
+    const option = {
+      noData: true,
+      mediaType: 'photo'
+    }
+
+    launchImageLibrary(option, (response) => {
+      if(response.didCancel) {
+        setAvatarUrl(null);
+        return;
+      } else if(response.error) {
+        console.log(response.error)
+      } else {
+        onUpdatePhotoProfile(response)
+          .then(() => {
+            uploadAvatarPosts();
+          })
+
+        setAvatarUrl(response.assets[0].uri);
+      }
+    });
+  } 
+
+  async function onUpdatePhotoProfile(response) {
+    const fileSource = response.assets[0].uri;
+
+    const storageRef = storage()
+      .ref('users')
+      .child(user.uid);
+
+    return await storageRef.putFile(fileSource);
+  }
+
+  async function uploadAvatarPosts() {
+    const storageRef = storage()
+      .ref('users')
+      .child(user.uid);
+
+    await storageRef
+      .getDownloadURL()
+      .then(async (urlImage) => {
+        const postDoc = await firestore()
+          .collection('posts')
+          .where('userId', '==', user.uid)
+          .get();
+
+        postDoc.forEach(async (post) => {
+          await firestore().collection('posts').doc(post.id).update({
+            avatarUrl: urlImage
+          })
+        });
+      })
+  }
+
+  async function handleUpdateProfile() {
+    if (newNameText.trim() === '') {
+      return;
+    }
+
+    await firestore()
+      .collection('users')
+      .doc(user.uid)
+      .update({
+        name: newNameText
+      });
+
+    const userPosts = await firestore()
+      .collection('posts')
+      .where('userId', '==', user.uid)
+      .get()
+
+    userPosts.forEach(async (doc) => {
+      await firestore()
+        .collection('posts')
+        .doc(doc.id)
+        .update({
+          author: newNameText
+        });
+    });
+
+    let tempUser = {
+      uid: user.uid,
+      name: newNameText,
+      email: user.email
+    };
+
+    setNewNameText('');
+    setUser(tempUser);
+    setUserInLocalStorage(tempUser);
+    setModalIsOpen(false);
   }
 
   if (fullLoading) {
@@ -22,7 +161,59 @@ export function Profile() {
     <ProfileContainer>
       <Header />
 
-      <Button title="Sair" onPress={handleSignOut} />
+      {avatarUrl ? (
+        <UploadImageButton onPress={handleUpdatePhotoProfile}>        
+          <UploadImageText>+</UploadImageText>
+
+          <UserAvatar
+            source={{ uri: avatarUrl }}
+          />
+        </UploadImageButton>
+      ) : (
+        <UploadImageButton onPress={handleUpdatePhotoProfile}>
+          <UploadImageText>+</UploadImageText>
+        </UploadImageButton>
+      )}
+
+      <UserNameText>{user.name}</UserNameText>
+
+      <UserEmailText>{user.email}</UserEmailText>
+
+      <Button bg="#428cfd" onPress={() => setModalIsOpen(true)}>
+        <ButtonText color="#fff">
+          Atualizar Perfil
+        </ButtonText>
+      </Button>
+
+      <Button bg="#ddd" onPress={handleSignOut}>
+        <ButtonText color="#353840">
+          Sair
+        </ButtonText>
+      </Button>
+
+      <Modal visible={modalIsOpen} animationType='slide' transparent={true}>
+        <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+          <BackButton onPress={() => setModalIsOpen(false)}>
+            <Feather name="arrow-left" size={24} color="#121212" />
+
+            <ButtonText color="#121212">
+              Voltar
+            </ButtonText>
+          </BackButton>
+
+          <Input
+            placeholder={user.name}
+            value={newNameText}
+            onChangeText={(text) => setNewNameText(text)}
+          />
+
+          <Button bg="#428cfd" onPress={handleUpdateProfile}>
+            <ButtonText color="#fff">
+              Atualizar
+            </ButtonText>
+          </Button>
+        </ModalContainer>
+      </Modal>
     </ProfileContainer>
   )
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,91 @@
 import styled from "styled-components/native";
 
-export const ProfileContainer = styled.View``;
+export const ProfileContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: #353840;
+`;
+
+export const UploadImageButton = styled.TouchableOpacity`
+  margin-top: 20%;
+  background-color: #fff;
+  width: 165px;
+  height: 165px;
+  border-radius: 90px;
+  align-items: center;
+  justify-content: center;
+  z-index: 9;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 160px;
+  height: 160px;
+  border-radius: 80px;
+`;
+
+export const UploadImageText = styled.Text`
+  font-size: 54px;
+  position: absolute;
+  color: #e52246;
+  opacity: 0.4;
+  z-index: 99;
+`;
+
+export const UserNameText = styled.Text`
+  margin: 20px 20px 0 20px;
+  font-size: 28px;
+  font-weight: bold;
+  color: #FFF;
+`;
+
+export const UserEmailText = styled.Text`
+  color: #fff;
+  margin: 10px 20px 0 20px;
+  font-size: 20px;
+  font-style: italic;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 16px;
+  background-color: ${props => props.bg};
+  width: 80%;
+  height: 50px;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 18px;
+  color: ${props => props.color};
+`;
+
+export const ModalContainer = styled.KeyboardAvoidingView` 
+  width: 100%;
+  height: 70%;
+  background-color: #fff;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 15px;
+  left: 25px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const Input = styled.TextInput`
+  background-color: #ddd;
+  width: 80%;
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 18px;
+  color: #121212;
+  text-align: center;
+`;
